cbd4f8b3adb257f4ee4239693cf89caae1ab0db1
JapSir <japsircode@gmail.com>
2022-10-29 00:00:00 +0200

    create basic styling

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 let bookDisplay = document.querySelector(".bookDisplay")
 let bookAmount = 0;
+let bookRead = [true, true, false, false, false, false]
+let buttons = []
 let LOFTR  = {
     title: "LOTR",
     author: "henk",
     pages: 69,
-    read: true
 }
 let potter  = {
     title: "Potter",
     author: "Erika",
     pages: 69,
-    read: true
 }
 let GVR  = {
     title: "GVR",
     author: "piet",
     pages: 69,
-    read: true
 }
 let SenW  = {
     title: "Suske en Wiske",
     author: "jan",
     pages: 69,
-    read: true
+}
+let Sent  = {
+    title: "Suske en Wiske",
+    author: "jan",
+    pages: 69,
+}
+let Seng  = {
+    title: "Suske en Wiske",
+    author: "jan",
+    pages: 69,
 }
 
-let myLibrary = [LOFTR, potter, GVR, SenW]
+let myLibrary = [LOFTR, potter, GVR, SenW, Sent, Seng]
 
 
 function Book(title, author, pages, read) {
@@ -42,7 +50,6 @@ function displayBooks() {
     let titleDivs = []
     let authorDivs = []
     let pagesDivs = []
-    let readDivs = []
     
     myLibrary.forEach(item => {
         //create divs for the books
@@ -59,26 +66,29 @@ function displayBooks() {
 
         authorDivs[counter] = document.createElement("div")
         authorDivs[counter].classList.add(`author${counter}`, "author")
-        authorDivs[counter].innerText = item.author
+        authorDivs[counter].innerText = `Author: ${item.author}`
         bookDivs[counter].appendChild(authorDivs[counter])
 
         pagesDivs[counter] = document.createElement("div")
         pagesDivs[counter].classList.add(`pages${counter}`, "pages")
-        pagesDivs[counter].innerText = item.pages
+        pagesDivs[counter].innerText = `Pages : ${item.pages}`
         bookDivs[counter].appendChild(pagesDivs[counter])
 
-        readDivs [counter] = document.createElement("div")
-        readDivs [counter].classList.add(`read${counter}`, "read")
-        readDivs [counter].innerText = item.read
-        bookDivs [counter].appendChild(readDivs[counter])
+        buttons[counter] = document.createElement("button")
+        buttons[counter].classList.add(`read${counter}`, "read")
+        if (bookRead[counter] === true) {
+            buttons[counter].innerText = "Book Read, Wel Done!"
+            buttons[counter].style.background = "green"
+        } else {
+            buttons[counter].innerText = "Book Not Read (yet)"
+            buttons[counter].style.background = "red"
+        }
+        
+        bookDivs[counter].appendChild(buttons[counter])
 
         
 
 
-        console.log(item.title)
-        console.log(item.author)
-        console.log(item.pages)
-        console.log(item.read)
         counter++
     })
 }
